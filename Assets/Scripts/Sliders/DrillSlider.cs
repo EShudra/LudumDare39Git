2488e07c6208e58ad517dbfa2696235ae4c945ca
Igor Shudra <shudra.ia@gmail.com>
2017-07-31 14:37:14 +0300

    Merge branch 'master' into Anglain

---
+++ Assets/Scripts/Sliders/DrillSlider.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillSlider : ControlSlider {
+
+	//player object to control
+	Drill drill;
+
+	//move key
+	public KeyCode drillOnOffKey;
+	public KeyCode drillMoveKey;
+
+	public override void Awake ()
+	{
+		//call parent awake
+		base.Awake ();
+
+		//find object with tag "player"
+		drill = GameObject.FindGameObjectWithTag ("Drill").GetComponent<Drill> ();
+
+		//check if player in null
+		if (drill == null) {
+			Debug.LogError ("No drill object found, or no Drill component is attached to the drill! [DRILL_SLIDER.CS]");
+		}
+
+	}
+
+	public override void EnablePlayerProperty (bool state)
+	{
+		drill.setDrillOnOff (state);
+	}
+
+	public override void SetPlayerProperty (float value)
+	{
+		drill.setDrillDpsFromSlider (getSliderValue ());
+	}
+
+	//when drill hotkeyDown is true drill switches state (On/Off)
+	public override bool GetInputAxis ()
+	{
+		return Input.GetKeyDown (drillOnOffKey);
+	}
+
+	public override void Update ()
+	{
+		base.Update ();
+		//spend power
+		if (drill.drillIsOn) {
+			float costMul = getSliderValue();
+			pBar.subtractPower (powerCost*Time.deltaTime*costMul);
+		}
+
+		if (Input.GetKeyDown(drillMoveKey)){
+			drill.toggleDrillAngle ();
+		}
+	}
+}
