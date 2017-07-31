b9d5ad39884cb15741721626b4875d1503c27cfd
Igor Shudra <shudra.ia@gmail.com>
2017-07-31 23:38:09 +0300

    Pickable equipment
     - Pickable gun and drill prefabs with animations
     - ShowItemsOnContact.cs
     - DestroyOnContact.cs

---
+++ Assets/Scripts/PickableItems/ShowItemsOnContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowItemsOnContact : MonoBehaviour {
+
+	[SerializeField] private GameObject[] objects;
+
+	private void Awake() {
+		SetStates(false);
+	}
+
+	private void OnTriggerEnter2D(Collider2D col) {
+		SetStates(true);
+	}
+
+	private void SetStates(bool state) {
+		foreach (GameObject go in objects) {
+			go.SetActive(state);
+		}
+	}
+}
