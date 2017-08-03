a2eb0417a4ca58163863496251a0ccda6eb2c7ad
EShudra <poorgenmail@rambler.ru>
2017-08-03 23:10:11 +0300

    final fixes

--- Assets/Scripts/FallDamage.cs
+++ Assets/Scripts/FallDamage.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class FallDamage : MonoBehaviour {
 
-	[SerializeField] private float rigidbodyDamageBound = -10f;
-	[SerializeField] private float fallDamageMultiplier = 0.1f;
+	//[SerializeField] private float rigidbodyDamageBound = -10f;
+	[SerializeField] private float raiseFallDamageSpeed = 10f;
+	[SerializeField] private float fallDamageMul = 3f;
+
+	[SerializeField] private float fallDamageThreshold = 0f;
 
 	private float fallDamage;
 	private bool gotDamage = false;
@@ -25,8 +28,13 @@ public class FallDamage : MonoBehaviour {
 		if (rb2D == null) {
 			Debug.LogError("No Rigidbody2D component attached to the parent object found! [FALL_DAMAGE.CS]");
 		}
+
+		lastY = player.transform.position.y;
 	}
-	
+
+	private float lastY;
+
+	/*
 	// Update is called once per frame
 	void Update () {
 		if (rb2D.velocity.y < rigidbodyDamageBound) {
@@ -34,12 +42,26 @@ public class FallDamage : MonoBehaviour {
 			fallDamage = player.fallDamage + (-rb2D.velocity.y - rigidbodyDamageBound) * fallDamageMultiplier;
 		}
 	}
-	
+
+	/*
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if (gotDamage) {
 			Debug.Log("You got hit by " + fallDamage + ". Are you still alive, bruh?");
 			player.Hit(fallDamage);
 			gotDamage = false;
 		}
+	}*/
+
+	void FixedUpdate(){
+		if ((player.transform.position.y >= lastY) && (fallDamage > fallDamageThreshold)) {
+			float correctDamage = fallDamageThreshold + (fallDamage - fallDamageThreshold)*fallDamageMul;
+			Debug.Log("You have fallen. Damage: " + correctDamage);
+			player.Hit(correctDamage);
+		}
+
+		fallDamage = (lastY - player.transform.position.y) * raiseFallDamageSpeed;
+
+		lastY = player.transform.position.y;
 	}
+
 }
